41b53d4c7dd85b4adca10c6b8de182f8556924fd
Justin Shaw <wyojustin@gmail.com>
2015-09-24 14:03:45 -0400

    got create_faceplate working from old code

--- fabricate/create_CG2_faceplate.py
+++ fabricate/create_CG2_faceplate.py
@@ -1,4 +1,4 @@
-# -*- coding: latin-1 -*-
+ # -*- coding: latin-1 -*-
 import shutil
 import string
 import os.path
@@ -20,11 +20,12 @@ from reportlab.pdfbase.ttfonts import TTFont
 import glob
 import os.path
 import sys
-
-install_dir = os.path.abspath(os.path.join(os.path.split(os.path.abspath(__file__))[0], '..'))
-sys.path.append(os.path.join(install_dir, 'fabricate', 'CNC'))
+sys.path.append('/home/justin/Dropbox/WyoLumCode/CNC/')
 from cnc import *
 from baffles import *
+
+sys.path.append('../')
+from cnc import *
 
 from numpy import arange
 from copy import deepcopy
@@ -58,7 +59,7 @@ linewidth = 1.
 fontnames = ['HELVETICA-BOLD', 'Helvetica-Bold',
              'HELVETICA', 'Helvetica',
              ]
-fontpath = ['/Users/Nick/Dropbox/WyoLumCode/fonts/', '/Users/justin/Documents/googlefontdirectory/']
+fontpath = ['/home/justin/Dropbox/WyoLumCode/fonts/', '/home/justin/Documents/googlefontdirectory/']
 
 BAFFLE_H = 20.00 * mm - 3.9 * mm
 BAFFLE_T = .076 * inch
@@ -117,7 +118,7 @@ def create_backplate(can):
     
     ## Draw finger hole
     finger_r = 8 * mm
-    p = MyPath(color=red)
+    p = MyPath()
     p.moveTo(-extra, HEIGHT/2 + finger_r)
     p.lineTo(p.getLast() + (finger_r, 0))
     center = p.getLast() - (0, finger_r)
@@ -136,14 +137,14 @@ def create_backplate(can):
     p.drawOn(can)
 
     if True:
-        keyhole = Keyhole([2 * inch, 8 * inch], color=red)
+        keyhole = Keyhole([2 * inch, 8 * inch])
         keyhole.drawOn(can, linewidth=linewidth)
-        keyhole = Keyhole([WIDTH - 2 * inch, 8 * inch], color=red)
+        keyhole = Keyhole([WIDTH - 2 * inch, 8 * inch])
         keyhole.drawOn(can, linewidth=linewidth)
 
     pcb = getPCB(outline=False, leds=False, buttons=False, button_holes=True)
     pcb.translate(x0 - dx/2, PCB_OFF)
-    pcb.drawOn(can, linewidth=linewidth, color=red)
+    pcb.drawOn(can, linewidth=linewidth)
 
     mounts = [
         [.15 * inch, .15 * inch, mount_r],
@@ -156,7 +157,7 @@ def create_backplate(can):
     #     pcb.drill(*hole)
     pcb.translate(x0 - dx/2, PCB_OFF)
     pcb.translate(pcb_w,0)
-    pcb.drawOn(can, linewidth=linewidth, color=red)
+    pcb.drawOn(can, linewidth=linewidth)
 
 def drawline(p, x0, y0, x1, y1, linewidth=linewidth):
     p.moveTo(x0, y0)
@@ -175,8 +176,8 @@ def add_font(fontname, path=None):
                     if FN[:-4] == FONTNAME and FN[-4:] == '.TTF':
                         pdfmetrics.registerFont(TTFont(FN[:-4], os.path.join(d, fn)))
                         fontnames.append(FONTNAME)
-                        if not os.path.exists(os.path.join('/Users/Nick/Dropbox/WyoLumCode/fonts/', fn)):
-                            shutil.copy(os.path.join(d, fn), os.path.join('/Users/Nick/Dropbox/WyoLumCode/fonts/', fn))
+                        if not os.path.exists(os.path.join('/home/justin/Dropbox/WyoLumCode/fonts/', fn)):
+                            shutil.copy(os.path.join(d, fn), os.path.join('/home/justin/Dropbox/WyoLumCode/fonts/', fn))
                         break
             for fontdir in fontpath:
                 os.path.walk(fontdir, addit, ())
@@ -186,20 +187,17 @@ def add_font(fontname, path=None):
             path = '%s/%s.ttf' % (path, fontname)
             pdfmetrics.registerFont(TTFont(FONTNAME, path))
             fontnames.append(FONTNAME)
-    if FONTNAME not in fontnames:
-        print fontnames
-        raise ValueError("could not install FONTNAME: %s" % FONTNAME)
     return FONTNAME in fontnames
 
 def new_canvas(basename):
     can = canvas.Canvas('%s/%s_%s.pdf' % (directory, basename, __version__),
                         pagesize=(WIDTH + 2 * MARGIN, HEIGHT + 2 * MARGIN))
     return can
-def outline(extra=0, color=red):
+def outline(extra=0):
     '''
     draw CG2 outline with "extra" margin for backplate tightness
     '''
-    p = MyPath(color=color)
+    p = MyPath()
     p.moveTo(-extra, -extra)
     p.lineTo(WIDTH + extra, -extra)
     p.lineTo(WIDTH + extra, HEIGHT + extra)
@@ -219,7 +217,7 @@ def getPCB(outline=False, leds=False, buttons=False, button_holes=True):
 
     led_xs = arange(nx) * dx + x0
     led_ys = (arange(ny) * dy + y0)[::-1]
-    pcb = MyPath(color=black)
+    pcb = MyPath()
     if outline:
         pcb.rect([0, 0,
                   pcb_w, pcb_h,])
@@ -252,25 +250,25 @@ def getPCB(outline=False, leds=False, buttons=False, button_holes=True):
             pcb.drill(x, y, 2.5 * mm) # BUTTON
         if button_holes:
             button_hole(x, y, pcb)
-            # pcb.addText(x, y + .25 * inch, labels[i], 'TERMINALDOSIS-MEDIUM', 12, color=pink)    
+            pcb.addText(x, y + .25 * inch, labels[i], 'TERMINALDOSIS-MEDIUM', 12)    
     ## thumbwheel
     x = pcb_w - .5 * inch - 5 * .6 * inch
     y = pcb_h - .5 * inch
     pcb.drill(x, y, .25 * inch)
-    # pcb.addText(x, pcb_h - .4 * inch + .25 * inch, 'DIM', 'TERMINALDOSIS-MEDIUM', 12, color=pink)
+    pcb.addText(x, pcb_h - .4 * inch + .25 * inch, 'DIM', 'TERMINALDOSIS-MEDIUM', 12)
     pcb.rect([pcb_w - .44 * inch - .62 * inch, .44 * inch,
               .62 * inch, .12 * inch])
     x = pcb_w - .44 * inch - .62 * inch
     y = .28 * inch
     # can.drawCentredString(y - .04 * inch, -x,'GRN')
-    # pcb.addText(x, y, 'G', 'TERMINALDOSIS-MEDIUM', 12, color=pink)
+    pcb.addText(x, y, 'G', 'TERMINALDOSIS-MEDIUM', 12)
     # can.drawCentredString(y, -x - .75 * inch,'BLK')
-    # pcb.addText(x + .65 * inch, y, 'B', 'TERMINALDOSIS-MEDIUM', 12, color=pink)
-    # pcb.addText(x + .6 * inch / 2, y + .35 * inch, '5V FTDI', 'TERMINALDOSIS-MEDIUM', 12, color=pink)
+    pcb.addText(x + .65 * inch, y, 'B', 'TERMINALDOSIS-MEDIUM', 12)
+    pcb.addText(x + .6 * inch / 2, y + .35 * inch, '5V FTDI', 'TERMINALDOSIS-MEDIUM', 12)
     return pcb
 
 def create_faceplate(basename, style, case, font, fontsize, reverse=True, color=None,
-                     can=None, showtime=False, who=None, baffles=False, do_corner_holes=True,
+                     can=None, showtime=False, who=None, baffles=False, do_corner_holes=False,
                      top=None,
                      bottom=None, fpid='NA'):
     if font.title().startswith('Helvetica'):
@@ -285,14 +283,30 @@ def create_faceplate(basename, style, case, font, fontsize, reverse=True, color=
         save_can = False
     can.saveState()
     
+    if save_can:
+        pass ## text only
+        # can.setFont('Courier', 18)
+        # can.drawCentredString(WIDTH / 2 + MARGIN, HEIGHT+1.5*MARGIN, "%s %s" % (basename, __version__))
+    if who:
+        pass
+        # can.drawCentredString(WIDTH / 2 + MARGIN, .5*MARGIN, who)
     can.setFont(font, fontsize)
     
-    can.drawString(MARGIN, HEIGHT + 3 * MARGIN / 2, font)
+    # can.drawString(MARGIN, MARGIN/4, "FPID: %s" % fpid)
+        
+    # can.drawString(MARGIN, HEIGHT + 3 * MARGIN / 2, font)
     if reverse:
         can.translate(WIDTH + 2 * MARGIN, 0)
         can.scale(-1, 1)
     else:
         can.drawCentredString(WIDTH / 2 + MARGIN, MARGIN/2, "SAMPLE ONLY, DO NOT PRINT")
+
+    if top:
+        # can.drawCentredString(WIDTH / 2 + MARGIN, HEIGHT - 1.3 * inch + MARGIN, top)
+        pass 
+    if bottom:
+        # can.drawCentredString(WIDTH / 2 + MARGIN, .9 * inch + MARGIN, bottom)
+        pass
 
     if color:
         can.setFillColor(color)
@@ -300,11 +314,8 @@ def create_faceplate(basename, style, case, font, fontsize, reverse=True, color=
         can.setFillColor(white)
 
     can.translate(MARGIN, MARGIN)
-    p = outline()
-    if do_corner_holes:
-        for hole in corner_holes:
-            p.drill(*hole)
-    p.drawOn(can, color=red)
+    # outline().drawOn(can)
+
     
     
     x0 = (WIDTH - 2 * pcb_w)/2 + .2 * inch
@@ -415,45 +426,38 @@ def my_lower(s):
 lower = my_lower
 upper = my_upper
 
-install_dir = os.path.abspath(os.path.join(os.path.split(os.path.abspath(__file__))[0], '..'))
-
-langs_path = os.path.join(install_dir, 'fabricate', 'Langs')
-english2_v1 = Langs.Simulate2x.readwtf(os.path.join(langs_path, "English2_v1.wtf"))['letters']
-# german2_v1440 = Langs.Simulate2x.readwtf(os.path.join(langs_path, "German2_v1440.csv"))['letters']
+english2_v1 = Langs.Simulate2x.readwtf("Langs/English2_v1.wtf")['letters']
+german2_v1440 = Langs.Simulate1440.readwtf("Langs/German2_v1440.csv")['letters']
 cases = {'lower':lower,
          'UPPER':upper}
 
 def main():
+    font = 'UbuntuMono-BoldItalic'
     font = 'AveriaSansLibre-Regular'
     font = 'TerminalDosis-Regular'
+    font = 'TerminalDosis-Medium'
     font = 'david'
     font = "Helvetica-Bold"
-    font = "JosefinSans-Regular"
-    font = 'TerminalDosis-Medium'
-    font = 'UbuntuMono-BoldItalic'
-    font = 'Ubuntu-M-hinting'
+    font = 'JosefinSans-Regular'
 
-    # add_font('TerminalDosis-Medium', os.path.join(install_dir, 'fabricate', 'fonts'))
-    # add_font('TerminalDosis-Regular', os.path.join(install_dir, 'fabricate', 'fonts'))
-    add_font(font, os.path.join(install_dir, 'fabricate', 'fonts'))
-
-    # case = 'UPPER'
+    add_font('TerminalDosis-Medium')
+    add_font('TerminalDosis-Regular')
+    add_font(font)
+    case = 'UPPER'
     case = 'lower'
     fontsize=35 # normal
     fontsize=28
-    fpid = 'Nick'
+    fpid = 'NONE'
     create_faceplate('english2_v1_%s_%s_%s' % (font, case, fontsize), 
                      english2_v1, 
                      # 'german2_v1440_%s_%s_%s' % (font, case, fontsize), 
                      # german2_v1440, 
                      cases[case], 
-                     font, fontsize, baffles=False, reverse=True, fpid=fpid,
-                     do_corner_holes=True,
-                     color=None)
+                     font, fontsize, baffles=False, reverse=True, fpid=fpid)
     bp_can = new_canvas('Backplate')
     create_backplate(bp_can)
-    # t = MyText(15.2 * inch, .85*inch, "MASTER", centered=False, color=pink)
-    # t.drawOn(bp_can)
+    t = MyText(15.2 * inch, .85*inch, "MASTER", centered=False)
+    t.drawOn(bp_can)
     bp_can.save()
     print 'wrote', bp_can._filename
     baff_can = new_canvas("Baffles")
